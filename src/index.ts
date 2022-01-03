4c85650c27b0de7ab963a4ada0df3887fcd9c9eb
yomarguti <yomar.guti@gmail.com>
2022-01-03 09:13:29 -0500

    Base User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hi there');
+import { User } from './models/User';
+
+const user = new User({ name: 'Yomar', age: 37 });
+console.log(user);
+
+user.set({ name: 'Carlos', age: 35 });
+
+user.on('change', () => {
+  console.log('Create User');
+});
+
+user.on('change', () => {
+  console.log('Save User');
+});
+
+user.trigger('change');
